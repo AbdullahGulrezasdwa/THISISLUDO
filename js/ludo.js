dd5f7a985d886fbcd957cfee1f44822553ce4a15
AuraFarmingPeople <abdullahgulrez123@gmail.com>
2026-02-09 12:26:39 +0400

    Update ludo.js

--- js/ludo.js
+++ js/ludo.js
@@ -1,200 +1,119 @@
-﻿// pasa count array
-var myArray = ["1", "2", "3", "4", "5", "6"];
+/* ===========================
+   MODERN PREMIUM LUDO ENGINE
+   =========================== */
 
-// globle pasa count 
-var pasaChal = '';
+/* GLOBAL STATE */
+let currentPlayer = 1;          // 1=red, 2=green, 3=blue, 4=yellow
+let diceValue = 0;
+let mustPlay = false;
 
-var katikholo = false;
+/* PLAYER CONFIG */
+const playerData = {
+    1: { color: "rd", start: "t14", entry: 1 },
+    2: { color: "gn", start: "t27", entry: 14 },
+    3: { color: "bl", start: "t1",  entry: 27 },
+    4: { color: "yl", start: "t40", entry: 40 }
+};
 
-var katiId = ''; 
+/* DICE CLICK */
+$("#dice").on("click", function () {
 
-var userHasBajji = 0;
-var katiRun = '';
-var katiStartFrom = '';
-var katiTurnFrom = '';
-var katiLal = '';
+    if (mustPlay) return; // must move before rolling again
 
+    diceValue = Math.floor(Math.random() * 6) + 1;
 
-$('body').on("click", "div.ludu_passas", function (e) {
+    $(this).attr("data-chal-count", diceValue);
 
-    pasaChal = myArray[Math.floor(Math.random() * myArray.length)];
+    // premium dice animation
+    $(this).addClass("dice-anim");
+    setTimeout(() => $(this).removeClass("dice-anim"), 450);
 
-    $(this).attr("data-chal-count", pasaChal);
-    katiRun = '';
-    die()
+    mustPlay = true;
 });
 
+/* TOKEN CLICK */
+$("body").on("click", ".kati", function () {
 
+    if (!mustPlay) return;
 
+    const token = $(this);
+    const tokenColor = token.attr("class").split(" ")[1];
+    const playerColor = playerData[currentPlayer].color;
 
-function die() {
+    if (tokenColor !== playerColor) return;
 
+    moveToken(token);
+});
 
-    if (userHasBajji == 4) {
+/* MOVE TOKEN */
+function moveToken(token) {
 
-        userHasBajji = 1;
+    const parent = token.parent().parent();
+    const isHome = parent.attr("data-kati-count") > 0;
 
+    if (isHome) {
+        openToken(token);
+        return;
     }
-    else {
-        userHasBajji++;
-    }
 
-
-    if (userHasBajji) {
-
-        if (userHasBajji == '1') {
-        
-            katiRun = "rd"
-            katiStartFrom = "t14"
-            katiTurnFrom = "t12"
-          }
-
-        if (userHasBajji == '2') {
-            katiRun = "gn";
-            katiStartFrom = "t27";
-            katiTurnFrom = "t25"
-        }
-
-        if (userHasBajji == '3') {
-            katiRun = "yl";
-            katiStartFrom = "t40";
-            katiTurnFrom = "t38"
-        }
-
-        if (userHasBajji == '4') {
-
-            katiRun = "bl";
-            katiStartFrom = "t1";
-            katiTurnFrom = "t51"
-        }
-
-    }
-    
-    alert('Kati Run =' + katiRun + ' katiStartFrom =' + katiStartFrom);
-    clcik()
-    
-   
+    moveOnBoard(token);
 }
 
-// Chal kati
-function clcik() {
-    $('body').on('click', '.kati', function (e) {
+/* OPEN TOKEN FROM HOME */
+function openToken(token) {
 
-        //alert($(this).hasClass(katiRun))
+    const p = currentPlayer;
+    const home = token.parent().parent();
+    let count = parseInt(home.attr("data-kati-count"));
 
-        if ($(this).hasClass(katiRun)) {
-            $thisId = $(this).attr('id');
-            console.log($thisId)
-            Chal($thisId, katiStartFrom, katiTurnFrom);
+    if (count <= 0) return;
 
-        }
+    const startCell = playerData[p].start;
 
-    });
+    $("#" + startCell).append(token.attr("data-step-count", 1));
+
+    count--;
+    home.attr("data-kati-count", count);
+
+    mustPlay = false;
+    nextTurn();
 }
 
- function Chal($thisId, katiStartFrom, katiTurnFrom) {
-        console.log($thisId)
-        //console.log(pasaChal);
+/* MOVE TOKEN ON BOARD */
+function moveOnBoard(token) {
 
-        katiId = $thisId;
-        var katiIcon = $('#' + katiId).prop('outerHTML');
-        //console.log(katiIcon);
+    const id = token.attr("id");
+    const currentCell = token.parent().attr("id");
+    const currentIndex = parseInt(currentCell.match(/\d+/)[0]);
+    let steps = parseInt(token.attr("data-step-count"));
 
-        
-        var boardAvailableKati = $('#' + katiId).parent().parent().attr('data-kati-count');
+    const newSteps = steps + diceValue;
 
-
-
-        if (boardAvailableKati > 0) {
-            if (pasaChal > 0) {
-
-
-                katikholo = true;
-
-                console.log(katiStartFrom);
-                katiKholore(katiStartFrom, katikholo, katiIcon);
-                boardAvailableKati = eval(boardAvailableKati) - 1;
-
-                $('#' + $thisId).parent().parent().attr('data-kati-count', boardAvailableKati)
-
-                console.log(boardAvailableKati)
-
-                $('#' + $thisId).remove();
-                return false;
-            }
-        }
-
-        else if (pasaChal > 0) {
-
-
-
-            // get track cloumn id
-
-            var theId = $('#' + $thisId).parent('.column').attr("id");
-            var theTracknum = theId.match(/\d+$/)[0];
-
-
-
-
-            // get set track stepsCount 
-
-            var forStepsCount = $('#' + katiId).attr('data-step-count');
-
-
-
-
-
-            forStepsCount = eval(forStepsCount) + eval(pasaChal);
-
-
-
-            $('#' + theId).children('#' + katiId).remove();
-
-            Chalar(pasaChal, katiId, katiIcon, theTracknum, forStepsCount);
-
-            pasaChal = '0';
-        }
+    if (newSteps > 56) {
+        console.log("Illegal move: cannot exceed final.");
+        return;
     }
 
+    const newIndex = currentIndex + diceValue;
 
+    // remove from old cell
+    $("#" + currentCell).children("#" + id).remove();
 
+    // choose path
+    if (newSteps > 51) {
+        $("#b" + newIndex).append(token);
+    } else {
+        $("#t" + newIndex).append(token);
+    }
 
+    token.attr("data-step-count", newSteps);
 
-
-
-// katikholo 
-function katiKholore(katiStartFrom, katiKholoHan, katiIcon) {
-    console.log(katiStartFrom);
-    $('#' + katiStartFrom).append($(katiIcon).attr('data-step-count', '1'));
-
-    katikholo = false;
-
+    mustPlay = false;
+    nextTurn();
 }
 
-// Chal
-
-function Chalar(chalCount, katiId, Chalkati, removeFrom, stepsCount) {
-    chalCount = eval(chalCount) + eval(removeFrom);
-
-    //console.log('chalCount' + chalCount);
-    //console.log('stepsCount' + stepsCount)
-
-
-    //if (stepsCount > 51) {
-    //    if (stepsCount > 56) {
-    //        alert('Passa is Not Valid for this Kati');
-    //        return false;
-    //    }
-    //    else {
-    //        $('#b' + chalCount).append(Chalkati);
-    //    }
-    //}
-    //else {
-
-    //    $('#t' + chalCount).append(Chalkati);
-
-    //}
-    $('#t' + chalCount).append(Chalkati);
-    $('#' + katiId).attr('data-step-count', stepsCount);
-
+/* NEXT TURN */
+function nextTurn() {
+    currentPlayer++;
+    if (currentPlayer > 4) currentPlayer = 1;
 }
